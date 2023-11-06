034f2b93e1fcd8661b03288b58e2950252dd23f4
Черкашин Ярослав Ю <iaiucherkashin@f-soft.org>
2023-11-06 22:13:53 +0300

    feat(login)

--- src/Auth/Models/User.php
+++ src/Auth/Models/User.php
@@ -44,6 +44,5 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
