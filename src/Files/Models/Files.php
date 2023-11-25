20720fad919766fb0c6d82dd38302f34f14966b7
Черкашин Ярослав Ю <iaiucherkashin@f-soft.org>
2023-11-25 16:27:37 +0300

    feat(Files): Add new namespace to composer.json

---
+++ src/Files/Models/Files.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Files\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Files extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'filename',
+        'filetype',
+        'filepage',
+    ];
+
+    public $timestamps = false;
+
+    protected $table = 'files';
+}
